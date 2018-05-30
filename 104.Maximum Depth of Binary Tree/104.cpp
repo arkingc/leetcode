93835d8d4583fff5caaf63708e661a061edeabd4
chenximing <243744327@qq.com>
2018-05-30 23:49:32 +0800

    104.二叉树的最大深度

---
+++ 104.Maximum Depth of Binary Tree/104.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)   return 0;
+        
+        return max(maxDepth(root->left) + 1,maxDepth(root->right) + 1);
+    }
+};
